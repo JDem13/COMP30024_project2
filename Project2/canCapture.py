9717aa4c52ef4331f3fad1a7222cae4647c5dae8
JDem13 <80367238+JDem13@users.noreply.github.com>
2022-05-08 08:04:58 +1000

    Update canCapture.py

--- Project2/canCapture.py
+++ Project2/canCapture.py
@@ -8,92 +8,115 @@ from inPath import withinBounds as wb
 
 def canCapture(board, n, node):
     
-    row = node[row]
-    column = node[column]
-    label = node[label]
-    canCapture = 1
-    cannotCapture = 0
+    row = node["row"]
+    column = node["column"]
+    label = node["label"]
+    canCapture = 0
+    capturedNodes= []
+    
+    if (label == "r"):
+        opposite_label = "b"
+    if (label == "b"):
+        opposite_label ="r"
+    
+    
     
          
     #outer sandwich case
     if(wb(n,row+1,column-1)*wb(n,row+1,column)*wb(n,row+2,column-1)):
-        if(board[(row+1,column-1)] != label):
-            if(board[(row+1,column)] != label):
+        if(board[(row+1,column-1)] == opposite_label):
+            if(board[(row+1,column)] == opposite_label):
                 if(board[(row+2,column-1)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row+1,column-1), (row+1, column))
+                    
     
     if(wb(n,row-1,column+1)*wb(n,row-1,column)*wb(n,row-2,column+1)):
-        if(board[(row-1,column+1)] != label):
-            if(board[(row-1,column)] != label):
+        if(board[(row-1,column+1)] == opposite_label):
+            if(board[(row-1,column)] == opposite_label):
                 if(board[(row-2,column+1)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row-1,column+1), (row-1, column))
                 
     if(wb(n,row+1,column)*wb(n,row,column+1)*wb(n,row+1,column+1)):
-        if(board[(row+1,column)] != label):
-            if(board[(row,column+1)] != label):
+        if(board[(row+1,column)] == opposite_label):
+            if(board[(row,column+1)] == opposite_label):
                 if(board[(row+1,column+1)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row+1,column), (row, column+1))
                 
     if(wb(n,row,column-1)*wb(n,row-1,column)*wb(n,row-1,column-1)):
-        if(board[(row,column-1)] != label):
-            if(board[(row-1,column)] != label):
+        if(board[(row,column-1)] == opposite_label):
+            if(board[(row-1,column)] == opposite_label):
                 if(board[(row-1,column-1)] == label):
-                    return canCapture    
+                    canCapture = 1
+                    capturedNodes.append((row,column-1), (row-1, column))    
                 
     if(wb(n,row,column-1)*wb(n,row+1,column-1)*wb(n,row+1,column-2)):
-        if(board[(row,column-1)] != label):
-            if(board[(row+1,column-1)] != label):
+        if(board[(row,column-1)] == opposite_label):
+            if(board[(row+1,column-1)] == opposite_label):
                 if(board[(row+1,column-2)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row,column-1), (row+1, column-1))
                 
     if(wb(n,row,column+1)*wb(n,row+1,column)*wb(n,row+1,column+1)):
-        if(board[(row,column+1)] != label):
-            if(board[(row+1,column)] != label):
+        if(board[(row,column+1)] == opposite_label):
+            if(board[(row+1,column)] == opposite_label):
                 if(board[(row+1,column+1)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row,column+1), (row+1, column))
             
     if(wb(n,row,column+1)*wb(n,row-1,column+1)*wb(n,row-1,column+2)):
-        if(board[(row,column+1)] != label):
-            if(board[(row-1,column+1)] != label):
+        if(board[(row,column+1)] == opposite_label):
+            if(board[(row-1,column+1)] == opposite_label):
                 if(board[(row-1,column+2)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row,column+1), (row-1, column+1))
                 
                 
     #inner sandwhich
     
     if(wb(n,row,column-1)*wb(n,row+1,column-1)*wb(n,row-1,column)):
-        if(board[(row-1,column)] != label):
-            if(board[(row+1,column-1)] != label):
+        if(board[(row-1,column)] == opposite_label):
+            if(board[(row+1,column-1)] == opposite_label):
                 if(board[(row,column-1)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row-1,column), (row+1, column-1))
             
     if(wb(n,row,column+1)*wb(n,row-1,column+1)*wb(n,row+1,column)):
-        if(board[(row+1,column)] != label):
-            if(board[(row-1,column+1)] != label):
+        if(board[(row+1,column)] == opposite_label):
+            if(board[(row-1,column+1)] == opposite_label):
                 if(board[(row,column+1)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row+1,column), (row-1, column+1))
                 
     if(wb(n,row+1,column)*wb(n,row+1,column-1)*wb(n,row,column+1)):
-        if(board[(row,column+1)] != label):
-            if(board[(row+1,column-1)] != label):
+        if(board[(row,column+1)] == opposite_label):
+            if(board[(row+1,column-1)] == opposite_label):
                 if(board[(row+1,column)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row,column+1), (row+1, column-1))
                 
     if(wb(n,row-1,column)*wb(n,row,column-1)*wb(n,row-1,column+1)):
-        if(board[(row-1,column+1)] != label):
-            if(board[(row,column-1)] != label):
+        if(board[(row-1,column+1)] == opposite_label):
+            if(board[(row,column-1)] == opposite_label):
                 if(board[(row-1,column)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row-1,column+1), (row, column-1))
                 
-    if(wb(n,row+1,colum-1)*wb(n,row+1,column)*wb(n,row,column-1)):
-        if(board[(row+1,column)] != label):
-            if(board[(row,column-1)] != label):
+    if(wb(n,row+1,column-1)*wb(n,row+1,column)*wb(n,row,column-1)):
+        if(board[(row+1,column)] == opposite_label):
+            if(board[(row,column-1)] == opposite_label):
                 if(board[(row+1,column-1)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row+1,column), (row, column-1))
                 
     if(wb(n,row-1,column+1)*wb(n,row,column+1)*wb(n,row-1,column)):
-        if(board[(row,column+1)] != label):
-            if(board[(row-1,column)] != label):
+        if(board[(row,column+1)] == opposite_label):
+            if(board[(row-1,column)] == opposite_label):
                 if(board[(row-1,column+1)] == label):
-                    return canCapture
+                    canCapture = 1
+                    capturedNodes.append((row,column+1), (row-1, column))
+    
+    return [canCapture, capturedNodes] 
                 
